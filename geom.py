4fa8fb49d698250ef565ce2ab8f10d9a639e6260
Dmitry Zinkevich <mr.zinch@protonmail.com>
2023-04-18 18:10:06 +0300

    Add vector to a point

--- geom.py
+++ geom.py
@@ -4,3 +4,8 @@ def point(x, y, z):
 def vector(x, y, z):
     return (x, y, z, 0)
 
+def add(t1, t2):
+     x1, y1, z1, w1 = t1
+     x2, y2, z2, w2 = t2
+     return (x1 + x2, y1 + y2, z1 + z2, w1 + w2)
+
--- test_geom.py
+++ test_geom.py
@@ -1,6 +1,6 @@
 import pytest
 
-from geom import point, vector
+from geom import *
 
 def test_point_creation():
     p = point(4, -4, 3)
@@ -9,4 +9,12 @@ def test_point_creation():
 def test_vector_creation():
     v = vector(4, -4, 3)
     assert v == (4, -4, 3, 0)
+
+def test_adding_two_tuples():
+    p = point(3, -2, 5)
+    v = vector(-2, 3, 1)
+
+    result = add(p, v)
+
+    assert result == (1, 1, 6, 1)
 
