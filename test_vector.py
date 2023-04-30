05073bfe83bd20e40161b3ba2199d1a8efabad03
Dmitry Zinkevich <mr.zinch@protonmail.com>
2023-04-30 23:16:35 +0300

    Extract vector tests to a separate file

---
+++ test_vector.py
@@ -0,0 +1,46 @@
+import pytest
+
+from geom import *
+from math_util import equal
+
+def test_negating_vector():
+    v = Vector(1, -2, 3)
+
+    result = -v
+
+    assert result.values == (-1, 2, -3, 0)
+
+def test_multiplying_vector_by_scalar():
+    v = Vector(1, -2, 3)
+
+    result = v * 3.5
+
+    assert result.values == (3.5, -7, 10.5, 0)
+
+def test_multiplying_vector_by_fraction():
+    v = Vector(1, -2, 3)
+
+    result = v * 0.5
+
+    assert result.values == (0.5, -1, 1.5, 0)
+
+@pytest.mark.parametrize('vector, expected_magnitude',
+        [
+            (Vector(1, 0, 0), 1),
+            (Vector(0, 1, 0), 1),
+            (Vector(0, 0, 1), 1),
+            (Vector(1, 2, 3), 3.74165),
+            (Vector(-1, -2, -3), 3.74165)
+        ])
+def test_magnitude(vector, expected_magnitude):
+    assert equal(vector.magnitude(), expected_magnitude)
+
+@pytest.mark.parametrize('vector, normalized_vector',
+        [
+            (Vector(4, 0, 0), Vector(1, 0, 0)),
+            (Vector(1, 2, 3), Vector(0.26726, 0.53452, 0.80178))
+        ])
+def test_magnitude(vector, normalized_vector):
+    assert vector.normalize() == normalized_vector
+    assert vector.normalize().magnitude() == 1
+
