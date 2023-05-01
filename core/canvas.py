418d5133d5fa328593b6024a682179ec8752a818
Dmitry Zinkevich <mr.zinch@protonmail.com>
2023-05-01 10:29:48 +0300

    Save canvas in PPM format

--- core/canvas.py
+++ core/canvas.py
@@ -1,3 +1,5 @@
+import math
+
 from core.color import Color
 
 class Canvas:
@@ -14,4 +16,28 @@ class Canvas:
         return self.pixels[self.index(x, y)]
 
     def index(self, x, y):
-        return y + self.height + x
+        return x + self.width * y
+
+def to_ppm_format(canvas):
+    def scale(value):
+        value = max(0, value)
+        return min(math.ceil(value * 255), 255)
+
+    lines = []
+    for j in range(0, canvas.height):
+        colors = []
+        for i in range(0, canvas.width):
+            p = canvas.pixel_at(i, j)
+            txt = f'{scale(p.red())} {scale(p.green())} {scale(p.blue())}'
+            colors.insert(i, txt)
+
+        lines.insert(j, ' '.join(colors))
+
+    lines = '\n'.join(lines)
+    return f'''
+P3
+{canvas.width} {canvas.height}
+255
+{lines}
+'''
+
--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -1,4 +1,4 @@
-from core.canvas import Canvas
+from core.canvas import Canvas, to_ppm_format
 from core.color import Color
 
 import pytest
@@ -19,3 +19,18 @@ def test_writing_pixel(canvas):
     assert canvas.pixel_at(0, 0) == Color(0, 0, 0)
     assert canvas.pixel_at(9, 19) == Color(0, 0, 0)
 
+def test_formatting_as_ppm():
+    canvas = Canvas(5, 3)
+    canvas.write_pixel(0, 0, Color(1.5, 0, 0))
+    canvas.write_pixel(2, 1, Color(0, 0.5, 0))
+    canvas.write_pixel(4, 2, Color(-0.5, 0, 1))
+
+    ppm = to_ppm_format(canvas)
+    assert ppm == '''
+P3
+5 3
+255
+255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 128 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
+'''
