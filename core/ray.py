9813bf4a6351ee250e3af71e5b8dea21a0d99b9f
Dmitry Zinkevich <mr.zinch@protonmail.com>
2023-05-06 18:07:12 +0300

    Compute distance from ray origin

--- core/ray.py
+++ core/ray.py
@@ -2,3 +2,6 @@ class Ray:
     def __init__(self, origin, direction):
         self.origin = origin
         self.direction = direction
+
+    def position(self, t):
+        return self.origin + self.direction * t
--- tests/test_ray.py
+++ tests/test_ray.py
@@ -9,3 +9,18 @@ def test_creating_ray():
 
     assert r.origin == origin
     assert r.direction == direction
+
+def test_computing_point_from_distance():
+    r = Ray(Point(2, 3, 4), Vector(1, 0, 0))
+
+    p0 = r.position(0)
+    assert p0 == Point(2, 3, 4)
+
+    p1 = r.position(1)
+    assert p1 == Point(3, 3, 4)
+
+    p_1 = r.position(-1)
+    assert p_1 == Point(1, 3, 4)
+
+    p = r.position(2.5)
+    assert p == Point(4.5, 3, 4)
