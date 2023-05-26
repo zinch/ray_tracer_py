19bf81a718bf54ad27bda3fe44acc959e7414786
Dmitry Zinkevich <mr.zinch@protonmail.com>
2023-05-26 22:04:41 +0300

    Compute normal vector of transformed sphere

--- core/objects.py
+++ core/objects.py
@@ -6,10 +6,18 @@ CENTER = Point(0, 0, 0)
 class Sphere:
     def __init__(self):
         self.transform = IDENTITY_MATRIX
+        self.inverse = IDENTITY_MATRIX
+        self.inverse_transpose = IDENTITY_MATRIX
 
     def set_transform(self, transform):
         self.transform = transform
+        self.inverse = transform.inverse()
+        self.inverse_transpose = self.inverse.transpose()
 
     def normal_at(self, point):
-        return (point - CENTER).normalize()
+        object_point = self.inverse * point
+        object_normal = (object_point - CENTER).normalize()
+        world_normal = self.inverse_transpose * object_normal
+        world_normal.w = 0
+        return world_normal.normalize()
 
--- tests/test_sphere.py
+++ tests/test_sphere.py
@@ -1,4 +1,5 @@
 from core.geom import Point, Vector
+from core.matrix import scaling, rotation_z, translation
 from core.objects import Sphere
 
 import math
@@ -32,3 +33,18 @@ def test_normal_is_normalized(sphere):
     n = sphere.normal_at(Point(math.sqrt(3) / 3, math.sqrt(3) / 3, math.sqrt(3) / 3))
 
     assert n == n.normalize()
+
+def test_computing_normal_on_translated_sphere(sphere):
+    sphere.set_transform(translation(0, 1, 0))
+
+    n = sphere.normal_at(Point(0, 1.70711, -0.70711))
+
+    assert n == Vector(0, 0.70711, -0.70711)
+
+def test_computing_normal_on_tranformed_sphere(sphere):
+    m = scaling(1, 0.5, 1) * rotation_z(math.pi / 5)
+    sphere.set_transform(m)
+
+    n = sphere.normal_at(Point(0, math.sqrt(2) / 2, -math.sqrt(2) / 2))
+
+    assert n == Vector(0, 0.97014, -0.24254)
